2293f2c63119d3257a9384cb8f0457850554ce59
A-Cobra <conradbravina@gmail.com>
2023-10-19 09:11:55 -0500

    created the main file

--- todo-app/index.js
+++ todo-app/index.js
@@ -7,6 +7,17 @@ const config = require("./config/config.js");
 const app = express();
 app.use(cors());
 
+const DATABASE_NAME = "todoapp";
+let database;
+
 app.listen(config.apiPort, () => {
+  console.log("Before mongo");
+  MongoClient.connect(config.mongoUri, (error, client) => {
+    database = client.db();
+    console.log("Mongo correctly connected");
+    if (error) {
+      console.log(error);
+    }
+  });
   console.log(`App listening on port ${config.apiPort}`);
 });
